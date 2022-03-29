47492fa50c3cd4b5174aa9d6e4616a04ec3c79c4
Rohan McLure <rmclure@linux.ibm.com>
2022-03-30 10:30:07 +1100

    Use macros for zeroing

--- arch/powerpc/include/asm/ppc_asm.h
+++ arch/powerpc/include/asm/ppc_asm.h
@@ -33,6 +33,14 @@
 	.endr
 .endm
 
+.macro SIMPLE_OP_REGS op, rhs, start, end
+  .Lreg=\start
+  .rept (\end - \start + 1)
+  \op .Lreg, \rhs
+  .Lreg=.Lreg+1
+  .endr
+.endm
+
 /*
  * Macros for storing registers into and loading registers from
  * exception frames.
@@ -45,9 +53,13 @@
 #else
 #define SAVE_GPRS(start, end, base)	OP_REGS stw, 4, start, end, base, GPR0
 #define REST_GPRS(start, end, base)	OP_REGS lwz, 4, start, end, base, GPR0
-#define SAVE_NVGPRS(base)		SAVE_GPRS(13, 31, base)
-#define REST_NVGPRS(base)		REST_GPRS(13, 31, base)
+#define SAVE_NVGPRS(base)		SAVE_GPRS(14, 31, base)
+#define REST_NVGPRS(base)		REST_GPRS(14, 31, base)
 #endif
+
+#define ZERO_GPRS(start, end)  SIMPLE_OP_REGS li, 0, start, end
+#define ZERO_NVGPRS() ZERO_GPRS(14,31)
+#define ZERO_GPR(n)   ZERO_GPRS(n, n)
 
 #define SAVE_GPR(n, base)		SAVE_GPRS(n, n, base)
 #define REST_GPR(n, base)		REST_GPRS(n, n, base)
