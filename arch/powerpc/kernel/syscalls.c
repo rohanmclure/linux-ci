12b1a2dd8f805b0267010c666dda470b31eebd95
Rohan McLure <rmclure@linux.ibm.com>
2022-04-01 11:27:13 +1100

    Eww gross

--- arch/powerpc/kernel/syscalls.c
+++ arch/powerpc/kernel/syscalls.c
@@ -78,9 +78,10 @@ int
 ppc_select(int n, fd_set __user *inp, fd_set __user *outp, fd_set __user *exp, struct __kernel_old_timeval __user *tvp)
 {
 	if ( (unsigned long)n >= 4096 )
-		return sys_old_select((void __user *)n);
+		return sys_old_select(__SC_LONG(int, n));
 
-	return sys_select(n, inp, outp, exp, tvp);
+	return sys_select(__MAP(5, __SC_LONG, int, n, fd_set __user *, inp, fd_set __user *, outp,
+			  fd_set __user *, exp, struct __kernel_old_timeval __user *, tvp));
 }
 #endif
 
