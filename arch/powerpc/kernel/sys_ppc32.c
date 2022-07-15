75c42fbfc24ffed88f3aa2f904c80cb2dbfdf1ec
Rohan McLure <rmclure@linux.ibm.com>
2022-07-15 12:28:23 +1000

    powerpc: Remove direct call to mmap2 syscall handlers
    Syscall handlers should not be invoked internally by their symbol names,
    as these symbols defined by the architecture-defined SYSCALL_DEFINE
    macro. Move the compatibility syscall definition for mmap2 to
    syscalls.c, so that all mmap implementations can share an inline helper
    function, as is done with the personality handlers.
    
    Signed-off-by: Rohan McLure <rmclure@linux.ibm.com>
    ---
    V1 -> V2: Move mmap2 compat implementation to asm/kernel/syscalls.c.
    V3 -> V4: Move to be applied before syscall wrapper introduced.

--- arch/powerpc/kernel/sys_ppc32.c
+++ arch/powerpc/kernel/sys_ppc32.c
@@ -25,7 +25,6 @@
 #include <linux/poll.h>
 #include <linux/personality.h>
 #include <linux/stat.h>
-#include <linux/mman.h>
 #include <linux/in.h>
 #include <linux/syscalls.h>
 #include <linux/unistd.h>
@@ -47,14 +46,6 @@
 #include <asm/ppc-pci.h>
 #include <asm/syscalls.h>
 #include <asm/switch_to.h>
-
-unsigned long compat_sys_mmap2(unsigned long addr, size_t len,
-			  unsigned long prot, unsigned long flags,
-			  unsigned long fd, unsigned long pgoff)
-{
-	/* This should remain 12 even if PAGE_SIZE changes */
-	return sys_mmap(addr, len, prot, flags, fd, pgoff << 12);
-}
 
 /* 
  * long long munging:
