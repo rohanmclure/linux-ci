48222ecc8538a4cd9964b1697c44b25f323bd199
Rohan McLure <rmclure@linux.ibm.com>
2022-08-30 11:23:56 +1000

    powerpc: mm: add p{te,md,ud}_user_accessible_page helpers
    Add the following helpers for detecting whether a page table entry
    is a leaf and is accessible to user space.
    
     * pte_user_accessible_page
     * pmd_user_accessible_page
     * pud_user_accessible_page
    
    Also implement missing definitions for both Book3S/E 64-bit systems.
    
    Signed-off-by: Rohan McLure <rmclure@linux.ibm.com>

--- arch/powerpc/include/asm/pgtable.h
+++ arch/powerpc/include/asm/pgtable.h
@@ -176,6 +176,39 @@ struct seq_file;
 void arch_report_meminfo(struct seq_file *m);
 #endif /* CONFIG_PPC64 */
 
+static inline bool pte_user_accessible_page(pte_t pte)
+{
+	return pte_present(pte) && pte_user(pte);
+}
+
+#ifdef CONFIG_PPC64
+
+static inline bool pmd_user_accessible_page(pmd_t pmd)
+{
+	return pmd_leaf(pmd) && pmd_present(pmd) && pmd_user(pmd);
+}
+
+static inline bool pud_user_accessible_page(pud_t pud)
+{
+	return pud_leaf(pud) && pud_present(pud) && pud_user(pud);
+}
+
+#else
+
+static inline bool pmd_user_accessible_page(pmd_t pmd)
+{
+	WARN(1, "pmd: multi-level paging unsupported on ppc32");
+	return false;
+}
+
+static inline bool pud_user_accessible_page(pud_t pud)
+{
+	WARN(1, "pud: multi-level paging unsupported on ppc32");
+	return false;
+}
+
+#endif /* CONFIG_PPC64 */
+
 #endif /* __ASSEMBLY__ */
 
 #endif /* _ASM_POWERPC_PGTABLE_H */
