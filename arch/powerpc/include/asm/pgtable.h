55506b5d7674ef4cfc65e58b29b75a5374b27253
Rohan McLure <rmclure@linux.ibm.com>
2022-08-30 11:23:56 +1000

    powerpc: mm: add p{te,md,ud}_user_accessible_page helpers
    Add the following helpers for detecting whether a page table entry
    is a leaf and is accessible to user space.
    
     * pte_user_accessible_page
     * pmd_user_accessible_page
     * pud_user_accessible_page
    
    The heavy lifting is done by pte_user, which checks user accessibility
    on a per-mmu level, provided prior to this commit.
    
    Signed-off-by: Rohan McLure <rmclure@linux.ibm.com>

--- arch/powerpc/include/asm/pgtable.h
+++ arch/powerpc/include/asm/pgtable.h
@@ -158,6 +158,28 @@ struct seq_file;
 void arch_report_meminfo(struct seq_file *m);
 #endif /* CONFIG_PPC64 */
 
+#ifdef ARCH_SUPPORTS_PAGE_TABLE_CHECK
+#define pte_user_accessible_page pte_user_accessible_page
+static inline bool pte_user_accessible_page(pte_t pte)
+{
+	return (pte_val(pte) & _PAGE_PRESENT) && pte_user(pte);
+}
+
+#define pmd_user_accessible_page pmd_user_accessible_page
+static inline bool pmd_user_accessible_page(pmd_t pmd)
+{
+	return pmd_is_leaf(pmd) && pmd_present(pmd)
+				&& pte_user(pmd_pte(pmd));
+}
+
+#define pud_user_accessible_page pud_user_accessible_page
+static inline bool pud_user_accessible_page(pud_t pud)
+{
+	return pud_is_leaf(pud) && pud_present(pud)
+				&& pte_user(pud_pte(pud));
+}
+#endif /* ARCH_SUPPORTS_PAGE_TABLE_CHECK */
+
 #endif /* __ASSEMBLY__ */
 
 #endif /* _ASM_POWERPC_PGTABLE_H */
