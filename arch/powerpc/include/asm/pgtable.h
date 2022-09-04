e5ab059a117cc977326e48f8086ab321e2c1295b
Rohan McLure <rmclure@linux.ibm.com>
2022-09-05 08:19:13 +1000

    Use pmd_is_leaf etc

--- arch/powerpc/include/asm/pgtable.h
+++ arch/powerpc/include/asm/pgtable.h
@@ -165,14 +165,14 @@ static inline bool pte_user_accessible_page(pte_t pte)
 
 static inline bool pmd_user_accessible_page(pmd_t pmd)
 {
-	return pmd_leaf(pmd) && (pmd_val(pmd) & _PAGE_PRESENT)
-			     && (pmd_val(pmd) & ~_PAGE_PRIVILEGED);
+	return pmd_is_leaf(pmd) && (pmd_val(pmd) & _PAGE_PRESENT)
+				&& (pmd_val(pmd) & ~_PAGE_PRIVILEGED);
 }
 
 static inline bool pud_user_accessible_page(pud_t pud)
 {
-	return pud_leaf(pud) && (pud_val(pud) & _PAGE_PRESENT)
-			     && (pud_val(pud) & ~_PAGE_PRIVILEGED);
+	return pud_is_leaf(pud) && (pud_val(pud) & _PAGE_PRESENT)
+				&& (pud_val(pud) & ~_PAGE_PRIVILEGED);
 }
 
 #endif /* __ASSEMBLY__ */
