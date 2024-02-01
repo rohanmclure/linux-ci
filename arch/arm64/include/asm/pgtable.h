a2f2ceca8d04c5e7152073f10f6e6ca451d9ca07
Rohan McLure <rmclure@linux.ibm.com>
2024-02-01 11:08:16 +1100

    Revert "mm/page_table_check: remove unused parameter in [__]page_table_check_pmd_set"
    riscv: Respect change to delete mm, addr parameters from __set_pte_at()
    
    This commit also changed calls to __set_pte_at() to use fewer parameters
    on riscv. Keep that change rather than reverting it, as the signature of
    __set_pte_at() is changed in a different commit.
    
    Signed-off-by: Rohan McLure <rmclure@linux.ibm.com>

--- arch/arm64/include/asm/pgtable.h
+++ arch/arm64/include/asm/pgtable.h
@@ -540,7 +540,7 @@ static inline void __set_pte_at(struct mm_struct *mm,
 static inline void set_pmd_at(struct mm_struct *mm, unsigned long addr,
 			      pmd_t *pmdp, pmd_t pmd)
 {
-	page_table_check_pmd_set(mm, pmdp, pmd);
+	page_table_check_pmd_set(mm, addr, pmdp, pmd);
 	return __set_pte_at(mm, addr, (pte_t *)pmdp, pmd_pte(pmd),
 						PMD_SIZE >> PAGE_SHIFT);
 }
@@ -1001,7 +1001,7 @@ static inline void pmdp_set_wrprotect(struct mm_struct *mm,
 static inline pmd_t pmdp_establish(struct vm_area_struct *vma,
 		unsigned long address, pmd_t *pmdp, pmd_t pmd)
 {
-	page_table_check_pmd_set(vma->vm_mm, pmdp, pmd);
+	page_table_check_pmd_set(vma->vm_mm, address, pmdp, pmd);
 	return __pmd(xchg_relaxed(&pmd_val(*pmdp), pmd_val(pmd)));
 }
 #endif
